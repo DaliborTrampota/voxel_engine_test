45101ad19a1b378835b8f595630f180fbc77a2e6
Dalibor Trampota <dalibor.trampota@autodesk.com>
2026-04-25 16:20:56 +0200

    set working directory on launch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <filesystem>
 
 #include <render/Window.h>
 #include "Game.h"
 
+#ifdef _WIN32
+#include "Windows.h"
+#endif
 
 // Force dedicated GPU (win only) (fixed bindless textures [probably laptop only issue?])
 #ifdef _WIN32
@@ -11,6 +15,20 @@ __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;  // for nv
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;    // for amd
 }
 #endif
+
+static void setWorkingDirToExe(const char* argv0) {
+#if defined(_WIN32)
+    char exePath[MAX_PATH];
+    GetModuleFileNameA(nullptr, exePath, MAX_PATH);
+    std::filesystem::current_path(std::filesystem::path(exePath).parent_path());
+#else
+    if (argv0) {
+        std::filesystem::current_path(
+            std::filesystem::canonical(std::filesystem::path(argv0)).parent_path()
+        );
+    }
+#endif
+}
 
 int gameEntry() {
     std::unique_ptr<engine::Window> window = std::make_unique<engine::Window>();
@@ -26,16 +44,15 @@ int gameEntry() {
 }
 
 #if defined(_WIN32)
-#include "Windows.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-    char exePath[MAX_PATH];
-    GetModuleFileNameA(nullptr, exePath, MAX_PATH);
+    setWorkingDirToExe(nullptr);
     return gameEntry();
 }
 
 #endif
 
 int main(int argc, char* argv[]) {
+    setWorkingDirToExe(argc > 0 ? argv[0] : nullptr);
     return gameEntry();
 }
